4e8411b507771ba32d1c617d2edba9bc33c14817
sahhoutiamine <midoriaduko@gmail.com>
2026-02-10 21:58:07 +0100

    Create Trip model

---
+++ app/Models/Trip.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'cheffeur_id',
+        'departure_city_id',
+        'arrival_city_id',
+        'departure_date',
+        'price_per_seat',
+        'status',
+        'available_seats',
+    ];
+
+    protected $casts = [
+        'departure_date' => 'datetime',
+    ];
+
+    public function cheffeur()
+    {
+        return $this->belongsTo(User::class, 'cheffeur_id');
+    }
+
+    public function departureCity()
+    {
+        return $this->belongsTo(City::class, 'departure_city_id');
+    }
+
+    public function arrivalCity()
+    {
+        return $this->belongsTo(City::class, 'arrival_city_id');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+}
