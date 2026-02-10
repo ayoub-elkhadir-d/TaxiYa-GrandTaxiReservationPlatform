5d65b1abf8ac503b72017b5925b094f8e6b09b6a
sahhoutiamine <midoriaduko@gmail.com>
2026-02-10 21:57:34 +0100

    Create Payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'reservation_id',
+        'amount',
+        'status',
+        'transaction_id',
+        'paid_at',
+    ];
+
+    protected $casts = [
+        'paid_at' => 'datetime',
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+}
