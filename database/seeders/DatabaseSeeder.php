29f635596107ca69995c196a2d696f0281306802
sahhoutiamine <midoriaduko@gmail.com>
2026-02-10 21:57:09 +0100

    Update DatabaseSeeder to call new seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CitySeeder::class,
+            UserSeeder::class,
+            TaxiSeeder::class,
+            TripSeeder::class,
+            ReservationSeeder::class,
+            PaymentSeeder::class,
         ]);
     }
 }
