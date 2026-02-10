f030ea76e669e78c55ab0ae9793537f4d94f08ba
sahhoutiamine <midoriaduko@gmail.com>
2026-02-10 21:58:28 +0100

    Create taxis table migration

---
+++ database/migrations/2026_02_10_202938_create_taxis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taxis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('matricule')->unique();
+            $table->string('model');
+            $table->string('color');
+            $table->string('car')->nullable(); // From Class Diagram 'car'
+            $table->integer('taxi_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taxis');
+    }
+};
