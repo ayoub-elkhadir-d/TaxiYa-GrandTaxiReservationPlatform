f3fd72a0f68760d91df1bdabb3b06556a7cb1a77
ayoub <assiacodi@gmail.com>
2026-02-11 11:11:03 +0100

    creation de function searche

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\City;
+use App\Models\Trip;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -11,5 +12,12 @@ class SearchController extends Controller
     public function index(){
         $cities = City::all();
         return view('home', compact('cities'));
+    }
+    public function searche(Request $R){
+       $DepartCity = $R->departcity;
+       $ArivaleCity = $R->arivalecity;
+       
+       $result=Trip::where('departure_city_id',$DepartCity)->where('arrival_city_id',$ArivaleCity)->get();
+       return view('search_results', compact('result'));
     }
 }
