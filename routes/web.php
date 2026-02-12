5c178cb19191d925d4058189748b2d6265ee482e
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-12 09:53:05 +0100

    add a route to confirm page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-<<<<<<< HEAD
-=======
-Route::get('/', ['App\Http\Controllers\SearchController', 'index'])->name('home');
-Route::get('/search', ['App\Http\Controllers\SearchController', 'search'])->name('search');
-Route::get('/test', ['App\Http\Controllers\SearchController', 'search'])->name('search');
->>>>>>> 6cf553d715abc6e506d76243e6b95422597789d0
+use App\Http\Controllers\SearchController;
 
 Route::get('/', [SearchController::class, 'index'])->name('home');
-Route::post('/search', [SearchController::class, 'searche'])->name('search');
+Route::get('/search', [SearchController::class, 'showCities'])->name('search');
+Route::post('/search', [SearchController::class, 'searche']);
+
+Route::get('/confirm', function (){
+    return view('traveler.confirm');
+});
