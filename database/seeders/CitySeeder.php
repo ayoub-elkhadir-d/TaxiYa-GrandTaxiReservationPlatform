55c69f09e3752d7217e35cab300b07ca327f54d3
sahhoutiamine <midoriaduko@gmail.com>
2026-02-13 09:47:20 +0100

    update the distance params in cities

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -11,19 +11,19 @@ class CitySeeder extends Seeder
     public function run(): void
     {
     $cities = [
-    ['name' => 'Casablanca', 'lat' => 33.58831, 'lng' => -7.61138],  
-    ['name' => 'Rabat', 'lat' => 34.01325, 'lng' => -6.83255],       
-    ['name' => 'Marrakech', 'lat' => 31.63416, 'lng' => -7.99994],
-    ['name' => 'Tangier', 'lat' => 35.76727, 'lng' => -5.79975],
-    ['name' => 'Agadir', 'lat' => 30.42018, 'lng' => -9.59815],
-    ['name' => 'Fes', 'lat' => 34.03313, 'lng' => -5.00028],
-    ['name' => 'Oujda', 'lat' => 34.686667, 'lng' => -1.911389],
-    ['name' => 'Essaouira', 'lat' => 31.5100, 'lng' => -9.7700],
-    ['name' => 'Safi', 'lat' => 32.29939, 'lng' => -9.23718],
-    ['name' => 'Beni Mellal', 'lat' => 32.33725, 'lng' => -6.34983],
-    ['name' => 'Nador', 'lat' => 35.16813, 'lng' => -2.93352],       
-    ['name' => 'Laayoune', 'lat' => 27.1212, 'lng' => -13.2034],      
-    ['name' => 'Dakhla', 'lat' => 30.41145, 'lng' => -9.55344],      
+    ['name' => 'Casablanca', 'x' => 33.58831, 'y' => -7.61138],  
+    ['name' => 'Rabat', 'x' => 34.01325, 'y' => -6.83255],       
+    ['name' => 'Marrakech', 'x' => 31.63416, 'y' => -7.99994],
+    ['name' => 'Tangier', 'x' => 35.76727, 'y' => -5.79975],
+    ['name' => 'Agadir', 'x' => 30.42018, 'y' => -9.59815],
+    ['name' => 'Fes', 'x' => 34.03313, 'y' => -5.00028],
+    ['name' => 'Oujda', 'x' => 34.686667, 'y' => -1.911389],
+    ['name' => 'Essaouira', 'x' => 31.5100, 'y' => -9.7700],
+    ['name' => 'Safi', 'x' => 32.29939, 'y' => -9.23718],
+    ['name' => 'Beni Mellal', 'x' => 32.33725, 'y' => -6.34983],
+    ['name' => 'Nador', 'x' => 35.16813, 'y' => -2.93352],       
+    ['name' => 'Laayoune', 'x' => 27.1212, 'y' => -13.2034],      
+    ['name' => 'Dakhla', 'x' => 30.41145, 'y' => -9.55344],      
 ];
 
         DB::table('cities')->upsert($cities, ['name'], ['x', 'y']);
